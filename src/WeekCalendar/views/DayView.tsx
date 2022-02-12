cada831f538d1ea0f785c01d0daad2a6bc766aad
eden lane <edencore@gmail.com>
2022-02-12 12:06:43 +0300

    hours

--- src/WeekCalendar/views/DayView.tsx
+++ src/WeekCalendar/views/DayView.tsx
@@ -14,91 +14,12 @@ import styled from "styled-components";
 import { useVirtualized } from "../hooks/useVirtualized";
 import { CalendarEvent } from "../types";
 
-const Days = styled.div`
-  height: 100%;
-  width: 100%;
-  display: flex;
-`;
-
-const Container = styled.div`
-  display: flex;
-  position: relative;
-  /* white-space: nowrap; */
-  flex-grow: 1;
-  overflow-x: scroll;
-  overflow-y: scroll;
-`;
-
-const Header = styled.header`
-  height: 60px;
-
-  white-space: nowrap;
-  position: sticky;
-  top: 0;
-  left: 0;
-  right: 0;
-  background: #fff;
-  z-index: 1;
-`;
-
-const Date = styled.div<{ width: number }>`
-  width: ${(p) => p.width}px;
-  display: inline-block;
-  padding: 1px 5px;
-  border-bottom: 1px solid #ccc;
-  flex-shrink: 0;
-
-  & div:nth-child(1) {
-    font-size: 12px;
-  }
-
-  & div:nth-child(2) {
-    font-size: 24px;
-  }
-`;
-
-const Day = styled.div<{ width: number }>`
-  width: ${(p) => p.width}px;
-  height: 100%;
-  display: inline-block;
-  flex-shrink: 0;
-
-  & + & {
-    border-left: 1px solid #ccc;
-  }
-`;
-
-const Aside = styled.aside<{ width: number }>`
-  width: ${(p) => p.width}px;
-  left: 0;
-  position: sticky;
-  flex: 0 0 60px;
-  padding: 5px;
-  z-index: 2;
-  border-right: 1px solid #ccc;
-`;
-
-const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const Hours = styled.div`
-  display: flex;
-`;
-
-const HOUR_HEIGHT = 100;
-
-const Hour = styled.div`
-  width: 100%;
-  height: ${HOUR_HEIGHT}px;
-  border-bottom: 1px solid #ccc;
-`;
-
 type Props = {
   range: Interval;
   events: CalendarEvent[];
 };
+
+const HOUR_HEIGHT = 100;
 
 export const DayView = (props: Props) => {
   const { range, events } = props;
@@ -157,11 +78,12 @@ export const DayView = (props: Props) => {
       <Days ref={rootRef}>
         <Container>
           <Aside width={width / visibleItems.length}>
+            <Header></Header>
             {eachHourOfInterval({
               start: startOfToday(),
               end: endOfToday()
             }).map((h) => (
-              <Hour key={h.toDateString()}>{format(h, "HH:mm")}</Hour>
+              <AsideHour key={h.toDateString()}>{format(h, "HH:mm")}</AsideHour>
             ))}
           </Aside>
           <Content style={{ width: containerStyle.size }}>
@@ -180,3 +102,91 @@ export const DayView = (props: Props) => {
     </>
   );
 };
+
+const Days = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+`;
+
+const Container = styled.div`
+  display: flex;
+  position: relative;
+  /* white-space: nowrap; */
+  flex-grow: 1;
+  overflow-x: scroll;
+  overflow-y: scroll;
+`;
+
+const Header = styled.header`
+  height: 50px;
+
+  white-space: nowrap;
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
+  background: #fff;
+  z-index: 1;
+`;
+
+const Date = styled.div<{ width: number }>`
+  width: ${(p) => p.width}px;
+  display: inline-block;
+  padding: 1px 5px;
+  border-bottom: 1px solid #ccc;
+  flex-shrink: 0;
+
+  & div:nth-child(1) {
+    font-size: 12px;
+  }
+
+  & div:nth-child(2) {
+    font-size: 24px;
+  }
+`;
+
+const Day = styled.div<{ width: number }>`
+  width: ${(p) => p.width}px;
+  height: 100%;
+  display: inline-block;
+  flex-shrink: 0;
+
+  & + & {
+    border-left: 1px solid #ccc;
+  }
+`;
+
+const Aside = styled.aside<{ width: number }>`
+  width: ${(p) => p.width}px;
+  left: 0;
+  position: sticky;
+  flex: 0 0 60px;
+  padding: 5px;
+  z-index: 2;
+  border-right: 1px solid #ccc;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Hours = styled.div`
+  display: flex;
+`;
+
+const Hour = styled.div`
+  width: 100%;
+  height: ${HOUR_HEIGHT}px;
+  border-bottom: 1px solid #ccc;
+`;
+
+const AsideHour = styled.div`
+  display: flex;
+  color: #555;
+  justify-content: center;
+  font-size: 12px;
+  height: ${HOUR_HEIGHT}px;
+  transform: translateY(-14px);
+`;
